bd6862a4828cc31d1073a463f01d64657fd5fdca
Andrey Denisov <and.denisov26@gmail.com>
2023-10-07 20:47:52 +0300

    Добавлен просмотр активных комнат для юзера

--- Net/chats/views.py
+++ Net/chats/views.py
@@ -6,13 +6,28 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 def index(request):
-    return render(request, "chats/index.html")
+    user = request.user.username
+    friends={}
+    active_rooms = Rooms.objects.filter(name__contains=user)
+    active_rooms = filter(lambda room_name: user == str(room_name).split('_')[0] or user == str(room_name).split('_')[1], active_rooms)
+    for room in active_rooms:
+        users = str(room.name).split('_')
+        if users[0] != user:
+            friend = User.objects.get(username=users[0])
+            friends[str(room.name)] = Profile.objects.get(user=friend.id)
+        else:
+            friend = User.objects.get(username=users[1])
+            friends[str(room.name)] = Profile.objects.get(user=friend.id)
+    data = {
+        'active_rooms': friends,
+    }
+    return render(request, "chats/index.html", data)
 
 def room(request, room_name):
     for n in room_name.split('_'):
         if n != request.user.username:
-            friend_username = User.objects.get(username__exact=n)
-            friend = Profile.objects.get(user__exact=friend_username.id)
+            friend_username = User.objects.get(username=n)
+            friend = Profile.objects.get(user=friend_username.id)
             break
     if Rooms.objects.filter(name=room_name):
         data = {
